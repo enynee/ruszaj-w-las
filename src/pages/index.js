2b08fb3e00ca3f378b94259c94bab34dd8bcdfbb
enynee <enynee@gmail.com>
2021-05-10 22:22:53 +0200

    index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,29 @@
 import React from "react"
 import Layout from "../components/layout"
+import lokalizacja from "../images/lokalizacja.png"
+import lupa from "../images/lupa.png"
+import odznakaterenowa from "../images/odznakaterenowa.png"
 
 export default function Home() {
-  return <Layout>Hello world!</Layout>
+  return <Layout>
+    
+  <div>
+      <p>Zdobądź Odnzakę Terenową</p>
+      <p>Odwiedź wszystkie lokalizacje w Trójmiejskim Parku Karjobrazowym. Podczas wycieczek zbieraj pieczątki, by zdobyć Odznakę Terenową</p>
+  </div>
+
+  <img src={lokalizacja}/>
+  <p>Odnajdź 12 punktów na terenie Trójmiejskiego Parku Krajobrazowego</p>
+
+  <img src={lupa}/>
+  <p>Zapoznaj się z cennymi informacjami przyrodniczymi</p>
+
+  <img src={odznakaterenowa}/>
+  <p>Zdobądź Odznakę terenową!</p>
+
+
+
+
+
+</Layout>
 }
--- src/pages/test.js
+++ src/pages/test.js
@@ -3,22 +3,19 @@ import Layout from "../components/layout"
 import wieza from "../images/wieza.png"
 
 export default function() {
-    let zdobyta = false;
     return <Layout>
-        <h1>Wieża Obserwacyjna Nr 1 w Ptasim Raju</h1>
+        <h1>Test</h1>
 
         <img src={wieza}/>
 
         <div>
             <p>Lokalizacja</p>
-            <p>Punkt znajduje się na Wyspie Sobieszewskiej w Rezerwacie Ptasi Raj</p>
+            <p>Punkt znajduje się w losowym miejscu</p>
         </div>
 
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2325.074805516368!2d18.800720316005584!3d54.355659106330684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTTCsDIxJzIwLjQiTiAxOMKwNDgnMTAuNSJF!5e0!3m2!1spl!2spl!4v1620554305728!5m2!1spl!2spl" width="600" height="450" allowfullscreen="" loading="lazy"></iframe>
 
-        { zdobyta === false && <p>Jesteś xm od celu</p> }
-        { zdobyta === true && <p>Jesteś u celu</p> }
-
+        <p>Jesteś xm od celu</p>
 
     </Layout>
 }
