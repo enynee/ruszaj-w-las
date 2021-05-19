4a9fe4e2664c0c265b90ae4763f75dfdf4afeca2
enynee <enynee@gmail.com>
2021-05-19 22:17:26 +0200

    header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,29 @@
 import React from "react"
 import "./layout.css"
+import logo from "../images/logo.png"
+import name from "../images/name.png"
 
 export default function Layout({ children }) {
   return <div>
-      <header>header</header>
-      <hr></hr>
+      <header>
+        <div id="logo">
+          <img src={logo} width="80" height="80"/>
+        </div>
+        <div id="name">
+          <img src={name} width="215" height="70"/>
+        </div>
+
+      </header>
+
+
       {children}
       <hr></hr>
-      <footer>footer</footer>
-      </div>
+      <footer>
+        <ul>
+          <li>Lista punktów</li>
+          <li>Odznaki</li>
+          <li>O projekcie</li>
+        </ul>
+      </footer>
+    </div>
 }
